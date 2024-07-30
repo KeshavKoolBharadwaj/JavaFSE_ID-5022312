5cc1fbb9dfce41fe27bbcceb3fe5dbe90de3ba79
Keshav Bharadwaj <101401559+KeshavKoolBharadwaj@users.noreply.github.com>
2024-07-30 17:15:10 +0530

    Add files via upload

---
+++ Week 1/Part 2/Exercise 8: Implementing the Strategy Pattern/StrategyPatternTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class StrategyPatternTest
+{
+    public static void main(String[] args)
+    {
+        PaymentContext paymentContext = new PaymentContext();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1 to pay with credit card");
+        System.out.println("Enter 2 tp pay with paypal");
+        System.out.print("Your choice: ");
+        int ch=sc.nextInt();
+        if(ch==1)
+        {
+            PaymentStrategy creditCardPayment = new CreditCardPayment("1234567890123456", "John Doe", "123", "12/24");
+            paymentContext.setPaymentStrategy(creditCardPayment);
+            paymentContext.pay(150.00);
+        }
+        else if (ch==2)
+        {
+            PaymentStrategy payPalPayment = new PayPalPayment("john.doe@example.com", "password");
+            paymentContext.setPaymentStrategy(payPalPayment);
+            paymentContext.pay(200.00);
+        }
+        else
+            System.out.println("Invalid payment type!");
+    }
+}
