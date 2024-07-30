2ba614348929835142e38b90294fac5a5f8263a3
Keshav Bharadwaj <101401559+KeshavKoolBharadwaj@users.noreply.github.com>
2024-07-30 17:02:48 +0530

    Add files via upload

---
+++ Week 1/Part 2/Exercise 7: Implementing the Observer Pattern/MobileApp.java
@@ -0,0 +1,26 @@
+public class MobileApp implements Observer
+{
+    private String appName;
+    public MobileApp(String appName)
+    {
+        this.appName = appName;
+    }
+    @Override
+    public void update(double stockPrice)
+    {
+        System.out.println(appName + " received stock price update: " + stockPrice);
+    }
+}
+class WebApp implements Observer
+{
+    private String appName;
+    public WebApp(String appName)
+    {
+        this.appName = appName;
+    }
+    @Override
+    public void update(double stockPrice)
+    {
+        System.out.println(appName + " received stock price update: " + stockPrice);
+    }
+}
