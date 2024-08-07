135fcb929f40cd9506e73df2fce7c1dccb7c9a52
Keshav Bharadwaj <101401559+KeshavKoolBharadwaj@users.noreply.github.com>
2024-08-07 09:14:44 +0530

    Add files via upload

---
+++ Week_2/Part_2_Spring_core_Maven/Exercise_2_Implementing_Dependency_Injection/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,16 @@
+package com.library;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class LibraryManagementApplication
+{
+    public static void main(String[] args)
+    {
+        // Load the Spring context from the XML configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // Retrieve the BookService bean from the context
+        BookService bookService = (BookService) context.getBean("bookService");
+        // Test the configuration by calling a method on the BookService
+        bookService.performService();
+    }
+}
